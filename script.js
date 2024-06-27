e334b07f3b4112d8849b42ec41c69dd9904fdc97
AlbertCoolGuy <39652844+AlbertCoolGuy@users.noreply.github.com>
2024-06-27 23:46:36 +0000

    added party mode

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function setRandomColors() {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   const randomColor2 = Math.floor(Math.random() * 16777215).toString(16);
-  console.log(randomColor);
   document.getElementById('hat').setAttribute('material', `${'color: ' + '#'}${randomColor}`);
   document.getElementById('rim').setAttribute('material', `${'color: ' + '#'}${randomColor2}; side: double`);
+}
+
+function partyMode() {
+  setInterval(
+    setRandomColors,
+    500
+  );
 }
 
 AFRAME.registerComponent('button', {
@@ -11,6 +17,15 @@ AFRAME.registerComponent('button', {
     const btn = document.getElementById('myButton');
     btn.addEventListener('click', () => {
       setRandomColors();
+    });
+  },
+});
+
+AFRAME.registerComponent('button2', {
+  init() {
+    const btn = document.getElementById('buttonParty');
+    btn.addEventListener('click', () => {
+      partyMode();
     });
   },
 });
